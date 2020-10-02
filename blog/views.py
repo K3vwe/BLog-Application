dea11c62a0dc6818783d74c5dffe1b32e8dad036
k3vwe <rukkimax@gmail.com>
2020-10-02 07:22:09 +0100

    add individual blog page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+# managing individual blog pages
+class BlogDetailView(DetailView):
+    # this is the database we are working on
+    model = Post
+    template_name = 'post_detail.html'
