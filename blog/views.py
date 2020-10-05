3449258420de8cdcb6ae0027c6ff16efdf7c8636
k3vwe <rukkimax@gmail.com>
2020-10-05 16:45:57 +0100

    Create a form to add and update blog post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
 
 # Create your views here.
@@ -12,3 +13,15 @@ class BlogDetailView(DetailView):
     # this is the database we are working on
     model = Post
     template_name = 'post_detail.html'
+
+# A Class to add new post
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+# A class to edit blog post
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'edit_post.html'
+    fields = ['title', 'body']
