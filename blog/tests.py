546aa5095e1d74bf5f882aaf800f53d1cc383e46
k3vwe <rukkimax@gmail.com>
2020-10-05 21:08:14 +0100

    add a login and logout form

--- blog/tests.py
+++ blog/tests.py
@@ -27,6 +27,9 @@ class BlogTest(TestCase):
         post = Post(title ='A simple title')
         self.assertEqual(str(post), post.title)
 
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
     # Test the blog content
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
@@ -47,4 +50,26 @@ class BlogTest(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text',
+        })
+        self.assertEqual(response.status_code, 302)
         
+    def test_post_delete_view(self):
+        response = self.client.get(
+            reverse('post_delete', args='1')
+        )
+        self.assertEqual(response.status_code, 200)
